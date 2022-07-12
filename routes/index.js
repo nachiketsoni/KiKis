8c9d4e608adc0fed5582614bd4bb5971469038a0
yash-mandlekar <ymandlekar9826@gmail.com>
2022-07-12 13:10:50 +0530

    yash

--- routes/index.js
+++ routes/index.js
@@ -31,14 +31,14 @@ router.get('/back', function (req, res) {
 
 router.post('/register', function (req, res) {
   var newUser = new userModel({
-    username: req.body.email,
+    username: req.body.username,
     name: req.body.name,
     mobilenumber: req.body.number,
   })
   userModel.register(newUser, req.body.password)
     .then(function (u) {
       passport.authenticate('local')(req, res, function () {
-        res.redirect('/profile');
+        res.redirect('/order');
       })
     })
 });
